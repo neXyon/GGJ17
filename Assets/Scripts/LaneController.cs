ea7f0092c6ae3213acf2dd40ecaa05501e7c64de
Jörg Müller <nexyon@gmail.com>
2017-01-21 20:59:39 +0100

    Music working.

--- Assets/Scripts/LaneController.cs
+++ Assets/Scripts/LaneController.cs
@@ -12,19 +12,22 @@ public class LaneController : MonoBehaviour {
   [SerializeField]
   private GameObject wavePrefab;
 
+  public static float[] FREQUENCIES = { 1, 1.12246205f, 1.33483985f, 1.49830708f, 1.68179283f };
+
   LaneController() {
     lanes = new GameObject[NUM_LANES];
   }
 
   private void Start() {
-    for(int i = 0; i < NUM_LANES; i++) {
+    for (int i = 0; i < NUM_LANES; i++) {
       lanes[i] = GameObject.Instantiate(wavePrefab);
       Lane lane = lanes[i].GetComponent<Lane>();
       //lane.wavePrefab = wavePrefab;
       //Lane lane = lanes[0].GetComponent<Lane>();
-      lane.Frequency = 1.5f;
+      lane.Frequency = FREQUENCIES[i];
       lane.Amplitude = 0.5f;
-      lanes[i].transform.position += new Vector3((i - 2) * 2.0f, 0);
+      lane.Width = 2.0f;
+      lanes[i].transform.position += new Vector3((i - 2) * 2.25f, 0);
     }
   }
 
---
+++ Assets/Scripts/Surfer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surfer : MonoBehaviour {
+  private int currentLane = 2;
+  private LaneController laneController;
+  private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+    laneController = GetComponent<LaneController>();
+    audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    int switchLane = -1;
+
+		if(Input.GetKeyDown(KeyCode.A)) {
+      switchLane = 0;
+    }
+    else if (Input.GetKeyDown(KeyCode.S)) {
+      switchLane = 1;
+    }
+    else if (Input.GetKeyDown(KeyCode.D)) {
+      switchLane = 2;
+    }
+    else if (Input.GetKeyDown(KeyCode.F)) {
+      switchLane = 3;
+    }
+    else if (Input.GetKeyDown(KeyCode.Space)) {
+      switchLane = 4;
+    }
+
+    if(switchLane != -1 && switchLane != currentLane) {
+      currentLane = switchLane;
+      Vector3 v = transform.position;
+      v.x = (currentLane - 2) * 2.25f;
+      transform.position = v;
+
+      audioSource.pitch = LaneController.FREQUENCIES[currentLane];
+    }
+  }
+}
